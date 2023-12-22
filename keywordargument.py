139f411d70bedafa4af9d1dffc23e596353ea773
oklabsfullstackmay2023 <oklabsfullstackmay2023@gmail.com>
2023-12-22 16:32:41 +0530

    arguments

---
+++ keywordargument.py
@@ -0,0 +1,7 @@
+#1.Function defination
+def hello(name, surname):
+    print(f"hello My Name is {name} and My Surname is {surname} ")
+
+#2.Function calling
+hello('Vishal', "Mahawar")      
+
---
+++ normalargument.py
@@ -0,0 +1,8 @@
+#1.Function Defination
+
+def hello(surname,name):
+    print(f'hello, My Name is {name} and My Surname is {surname} ')
+
+#2.Function calling
+
+hello("Vishal", "Mahawar")   
